08b32e86bbd6c7361e5fd7a2276aaac7a15e7b47
Christoph Rohrer <christoph@rohrers.de>
2024-09-28 12:10:16 +0200

    Initial Commit

---
+++ SupabaseDemo/SupabaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseDemoApp.swift
+//  SupabaseDemo
+//
+//  Created by Christoph Rohrer on 28.09.24.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
